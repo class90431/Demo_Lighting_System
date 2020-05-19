ceaa0c42fce59f6a5d9c9ae47f1f595aefe76ff0
Zack <108276@acbel.com>
2020-05-19 08:32:20 +0800

    set router & delete unnecessary file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,16 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Map from '../views/Map.vue'
 Vue.use(VueRouter)
 
-  const routes: Array<RouteConfig> = [
+const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
+    name: 'Map',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: Map
   }
 ]
 
